9659267a003dc40a21f62bdb95034ce0bf5fd129
lutao <lt8804258@163.com>
2017-01-07 21:45:32 +0800

    add gubenjiegou of version 1.0

--- gubenjiegou.js
+++ gubenjiegou.js
@@ -3,7 +3,7 @@
 const util = require('./common/util');
 const fs = require('fs');
 const async = require('async');
-
+const _ = require('lodash');
 gubenjiegou();
 
 function gubenjiegou(){
@@ -88,9 +88,7 @@ function getContent(token, list, cb){
 		}
 		async.waterfall([
 			(__cb) => {
-				let url = "https://gw.yundzh.com/f10/gbfh/gbjg?obj=";
-				let link = url + stock + '&token=' + token;
-				getGubenjiegou(link, __cb);
+				getGubenjiegou(stock, __cb);
 			},
 			(guben, __cb) => {
 				myData.gubenjiegou = guben;
@@ -100,13 +98,12 @@ function getContent(token, list, cb){
 			},
 			(xianshoujiejin, __cb) => {
 				myData.xianshoujiejin = xianshoujiejin;
-				let url = "https://gw.yundzh.com/f10/gdjc/gdhs?obj=";
-				let link = url + stock + '&token=' + token;
-				getGudongmingxi(link, __cb);
+				getGudongmingxi(stock, __cb);
 			},
 			(gudongmingxi, __cb) => {
 				myData.gudongmingxi = gudongmingxi;
-				let outputFilename = '../f10/gubenjiegou/' + stock + '.json';
+				//console.log(JSON.stringify(myData, null, 4))
+				let outputFilename = '../../f10/gubenjiegou/' + stock + '.json';
 				fs.writeFile(outputFilename, JSON.stringify(myData, null, 4), __cb);
 			}], (err) => {
 				if (err) return _cb(err);
@@ -121,35 +118,39 @@ function getContent(token, list, cb){
 }
 
 
-function getGubenjiegou(url, cb) {
+function getGubenjiegou(stock, cb) {
 	
+	stock = util.stockDZH2HS(stock);
+	if (!stock) {
+		return cb('stock code error');
+	}
+	let url = 'http://www.onehou.com/api/hs/info/v2/query/f10_equity_structure?en_prod_code=' + stock;
 	util.getData(url, (err, data) => {
-			
-		if (err) {
+	if (err) {
 			return cb(err);
 		} else if (!data) {
 			return cb('no data');
-		} else if (data.Err) {
-			return cb(data.Err);
 		} else if (typeof data == 'string') {
 			try {
 				data = JSON.parse(data);
 			} catch (e) {
-				return cb(e)
+				console.log(url, e)
+				return cb(null, {})
 			}
 		}
 			
-		if (!data.Data || !data.Data.RepDataF10GbfhGbjgOutput || !data.Data.RepDataF10GbfhGbjgOutput[0]){
+		if (!data.data || !data.data[0]){
 			console.log(url, data,'data exception');
 			return cb(null, {});
 		}
 		
-		var infos = data.Data.RepDataF10GbfhGbjgOutput[0].Data;
-		if (infos.length < 1) {
-			return cb('no data')
-		}
-		var info = infos[infos.length - 1];
-		cb(null, {Zgb: info.Zgb, Ltgf: info.Ltgf, Xsltg: info.Xsltg});
+		let date = _.keys(data.data[0])[0];
+		let info = data.data[0][date][0][stock][0];
+		cb(null, {
+			total_shares : util.isNumber(info.total_shares.split(',').join('')),
+			a_floats_shares : util.isNumber(info.a_floats_shares.split(',').join('')),
+			b_floats_shares : util.isNumber(info.b_floats_shares.split(',').join(''))
+		});
 	});
 }
 
@@ -181,43 +182,56 @@ function getXianshoujiejin(url, cb) {
 			infos.forEach(function(item) {
 				var info = {};
 				info.date = item.date;
-				info.jjgf = item.jjgf;
-				info.zzgf = item.zzgf;
+				info.jjgf = util.isBigNumber(item.jjgf);
+				info.zzgf = util.isNull(item.zzgf, '%');
 				list.push(info);
 			})
 			cb(null, list);
 		});
 }
 
-function getGudongmingxi(url, cb) {
+function getGudongmingxi(stock, cb) {
 	
+	stock = util.stockDZH2HS(stock);
+	if (!stock) {
+		return cb('stock code error');
+	}
+	let url = 'http://www.onehou.com/api/hs/info/v2/query/f10_shareholders?en_prod_code=' + stock;
 	util.getData(url, (err, data) => {
-			
-		if (err) {
+
+	if (err) {
 			return cb(err);
 		} else if (!data) {
 			return cb('no data');
-		} else if (data.Err) {
-			return cb(data.Err);
 		} else if (typeof data == 'string') {
 			try {
 				data = JSON.parse(data);
 			} catch (e) {
-				return cb(e)
+				console.log(url, e)
+				return cb(null, [])
 			}
 		}
 			
-		if (!data.Data || !data.Data.RepDataF10GdjcGdhsOutput || !data.Data.RepDataF10GdjcGdhsOutput[0]){
+		if (!data.data || !data.data[0]){
 			console.log(url, data,'data exception');
-			return cb(null, {});
+			return cb(null, []);
 		}
 		
-		var infos = data.Data.RepDataF10GdjcGdhsOutput[0].Data;
-		if (!infos || infos.length < 1) {
-			console.log(infos, 'special data');
-			return cb(null, {});
+		let date = _.keys(data.data[0])[0];
+		let info = data.data[0][date][0][stock];
+		let list = [];
+		for(let i = 0 ; i < info.length ; i++ ) {
+			let item = {};
+			if (i < info.length - 1 && info[i+1].ash_num) {
+				item.ash_inc_per = util.isNull(((info[i].ash_num - info[i+1].ash_num)/info[i+1].ash_num*100).toFixed(2), '%');
+			} else {
+				item.ash_inc_per = '--';
+			}
+			item.ash_num = info[i].ash_num;
+			item.report_date = info[i].report_date;
+			item.a_average_hold_sum = info[i].a_average_hold_sum;
+			list.push(item);
 		}
-		var info = infos[infos.length - 1];
-		cb(null, {date: info.date, Gdzhs: info.Gdzhs, Hbbh: info.Hbbh, Rjcg: info.Rjcg});
+		cb(null, list);
 	});
 }
